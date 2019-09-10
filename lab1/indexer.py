287ecbb56f70b051f872f9f1e89184960b2e3fb0
danielaahlin <daniel.ahlin96@gmail.com>
2019-09-10 22:37:05 +0200

    Fixfixfix

--- lab1/indexer.py
+++ lab1/indexer.py
@@ -21,7 +21,7 @@ def get_files(dir, suffix):
 def dictionaries(filepath):
     dic = {}
     with open(filepath, 'r') as file:
-        match = re.finditer('\w+', file.read().lower())
+        match = re.finditer('\p{L}+', file.read().lower())
         for m in match:
             word = m.group()
             dic[word] = [*dic[word], m.start()] if word in dic else [m.start()]
@@ -50,7 +50,7 @@ def idf(N, t, D):
     nt = float(len(D[t]))
     if nt == 0.0:
         nt = 1.0
-    return math.log10(float(N)/nt)
+    return np.log10(float(N)/nt)
 
 def tf_idf(t, d, N, D, sum):
     return tf(t, d, sum) * idf(N, t, D)
@@ -67,15 +67,34 @@ def main(folder):
     for t in txt_files:
         dic = dictionaries('{}/{}'.format(folder, t))
         for d in dic:
+            if t == 'bannlyst.txt':
+                if d == 'gjord' or d == 'uppklarnande' or d == 'stjärnor':
+                    print('{}: {}'.format(d, dic[d]))
             if d in master_dict:
                 master_dict[d].update({t: dic[d]})
             else:
                 master_dict[d] = {t: dic[d]}
 
+    print(10 * '-')
+    print('{}: {}'.format('samlar',master_dict['samlar']))
+    print('{}: {}'.format('ände',master_dict['ände']))
+    print(10 * '-')
+
     array_txt = []
     for t in txt_files:
-        dic = dictionaries('{}/{}'.format(folder, t))
-        array_txt.append(np.array([tf_idf(word, dic, len(txt_files), master_dict, len(re.findall('\w+', '{}/{}'.format(folder, t)))) for word in master_dict]))        
+        dic = dictionaries('{}/{}'.format(folder, t))   
+        #sum_of_words = len(re.findall('\p{L}+', '{}/{}'.format(folder, t)))
+        sum_of_words = 0
+        for d in dic:
+            sum_of_words += len(dic[d])
+        array_txt.append(np.array([tf_idf(word, dic, len(txt_files), master_dict, sum_of_words) for word in master_dict]))
+        if t == 'bannlyst.txt' or t == 'gosta.txt' or t == 'herrgard.txt' or t == 'jerusalem.txt' or t == 'nils.txt':
+            print(t)
+            print('    {} {}'.format('känna',   tf_idf('känna', dic, len(txt_files), master_dict, sum_of_words)))
+            print('    {} {}'.format('gås',     tf_idf('gås',   dic, len(txt_files), master_dict, sum_of_words)))
+            print('    {} {}'.format('nils',    tf_idf('nils',  dic, len(txt_files), master_dict, sum_of_words)))
+            print('    {} {}'.format('et',      tf_idf('et',    dic, len(txt_files), master_dict, sum_of_words)))
+    print(10 * '-')
 
     matrix = []
     for x in range(len(array_txt)):
@@ -83,8 +102,6 @@ def main(folder):
         for y in range(len(array_txt)):
             row.append(cosine_similarity(array_txt[x], array_txt[y]))
         matrix.append(row)
-    for m in matrix:
-        print(m)
 
         
 if __name__ == "__main__":
