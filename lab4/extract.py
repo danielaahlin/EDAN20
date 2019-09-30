8451ea8a710093fcad78e4b4af3296bc39ba8bfe
danielaahlin <daniel.ahlin96@gmail.com>
2019-09-30 16:29:09 +0200

    first part of lab done

--- lab4/extract.py
+++ lab4/extract.py
@@ -18,48 +18,28 @@ def extract_pairs(file):
 
 def extract_triples(file):
     triples = {}
-    sentence = [] 
-    #count = 0
+    sentence = []
+    ss_found = []
+    oo_found = []
     for f in file:
         line = f.split()
         if line == []:
             for s in sentence:
-                if s[7] == 'OO':
-                    if sentence[int(s[6]) - 1][7] == 'SS':
-                        index = int(sentence[int(s[6]) - 1][6])
-                        if index == 0:
-                            pass
-                        else:
-                            trip = (s[1].lower(), sentence[int(s[6]) - 1][1].lower() ,sentence[index - 1][1].lower())
+                if s[7] == 'SS':
+                    ss_found.append((int(s[0]) - 1, int(s[6]) - 1))
+                elif s[7] == 'OO':
+                    oo_found.append((int(s[0]) - 1, int(s[6]) - 1))
+            for ss in ss_found:
+                for oo in oo_found:
+                    if ss[1] == oo[1]:
+                        trip = (sentence[ss[1]][1].lower(), sentence[ss[0]][1].lower(), sentence[oo[0]][1].lower())
                         add_trip(trip, triples)
-                    else: 
-                        index = int(sentence[int(s[6]) - 1][6])
-                        if index == 0:
-                            #hita ss som pekar på root
-                            for ss in sentence:
-                                if ss[7] == 'SS':
-                                    idx =int(ss[6]) - 1
-                                    if int(sentence[idx][6]) == 0:
-                                        trip = (s[1].lower(), sentence[idx][1].lower(), ss[1].lower() ) 
-                                        add_trip(trip, triples)
-
-                        elif int(sentence[int(index) - 1][6]) == 0:
-                            pass
-                        elif sentence[int(index) - 1][7] == 'SS':
-                            trip = (s[1].lower(), sentence[int(s[6]) - 1][1].lower() ,sentence[index - 1][1].lower())
-                            add_trip(trip, triples)
-
-                elif s[7] == 'SS':
-
-
-
-
+            ss_found = []
+            oo_found = []
             sentence = []
         else:
             sentence.append(line)
     return triples
-    
-
 
 def add_trip(trip, triples):
     if trip in triples:
@@ -68,17 +48,25 @@ def add_trip(trip, triples):
         triples.update({trip : 1})
 
 if __name__ == "__main__":
+    #pairs
     with open('swedish_talbanken05_train.conll', 'r') as file:
-        #pairs
-        #pairs = extract_pairs(file)
-        #sorted_pairs = sorted(pairs.items(), key=lambda kv: kv[1])
-        #five_most_frqnt_pairs = [i[1] for i in sorted_pairs[-5:]]
-        #five_most_frqnt_pairs.reverse()
-        #print(five_most_frqnt_pairs)
-        #triples
+        count = 0
+        pairs = extract_pairs(file)
+        for p in pairs:
+            count += pairs[p]
+        print(count)
+        sorted_pairs = sorted(pairs.items(), key=lambda kv: kv[1])
+        five_most_frqnt_pairs = [i[1] for i in sorted_pairs[-5:]]
+        five_most_frqnt_pairs.reverse()
+        print(five_most_frqnt_pairs)
+    #triples
+    with open('swedish_talbanken05_train.conll', 'r') as file:
         count = 0
         triples = extract_triples(file)
         for t in triples:
             count += triples[t]
-
         print(count)
+        sorted_triples = sorted(triples.items(), key=lambda kv: kv[1])
+        five_most_frqnt_triples = [i[1] for i in sorted_triples[-5:]]
+        five_most_frqnt_triples.reverse()
+        print(five_most_frqnt_triples)
