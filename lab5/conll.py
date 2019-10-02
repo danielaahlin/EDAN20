8535fbd1594912d497ea5fb53236605e35158d0b
danielaahlin <daniel.ahlin96@gmail.com>
2019-10-02 12:32:49 +0200

    paths fixed, run in lab5 dir

--- lab5/conll.py
+++ lab5/conll.py
@@ -77,9 +77,9 @@ def save(file, formatted_corpus, column_names):
 if __name__ == '__main__':
     column_names_2006 = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats', 'head', 'deprel', 'phead', 'pdeprel']
 
-    train_file = '../../corpus/conllx/sv/swedish_talbanken05_train.conll'
+    train_file = '../lab4/swedish_talbanken05_train.conll'
     # train_file = 'test_x'
-    test_file = '../../corpus/conllx/sv/swedish_talbanken05_test.conll'
+    test_file = '../lab4/swedish_talbanken05_test.conll'
 
     sentences = read_sentences(train_file)
     formatted_corpus = split_rows(sentences, column_names_2006)
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 
     column_names_u = ['id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc']
 
-    files = get_files('../../corpus/ud-treebanks-v1.3/', 'train.conllu')
+    files = get_files('../lab4/ud-treebanks-v2.4/', 'train.conllu')
     for train_file in files:
         sentences = read_sentences(train_file)
         formatted_corpus = split_rows(sentences, column_names_u)
