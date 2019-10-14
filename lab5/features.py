09fedfd124ce8ace269af631ad3773d6160e1270
danielaahlin <daniel.ahlin96@gmail.com>
2019-10-14 13:46:12 +0200

    baseline added

---
+++ lab5/features.py
@@ -0,0 +1,16 @@
+"""
+First set  - POS_stack, POS_queue, word_stack, word_queue, can-la, can-re
+Second set - POS_stack0, POS_stack1, word_stack0, word_stack1, POS_queue0, POS_queue1, word_queue0, word_queue1, can-la, can-re
+Third set  - POS_stack0, POS_stack1, word_stack0, word_stack1, POS_queue0, POS_queue1, word_queue0, word_queue1, can-la, can-re,
+             POS_sentece_order, wordform_sentence_order
+"""
+
+def extract(stack, queue, graph, feature_names, sentence):
+    features = {}
+    
+
+    return features
+
+
+if __name__ == "__main__":
+    pass
