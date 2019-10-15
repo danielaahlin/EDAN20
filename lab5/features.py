14e0b2a53971bfd35be411c7e49bfffa2c831c56
danielaahlin <daniel.ahlin96@gmail.com>
2019-10-15 09:10:03 +0200

    koda sadan

--- lab5/features.py
+++ lab5/features.py
@@ -10,60 +10,78 @@ def extract(stack, queue, graph, feature_names, sentence, model_set):
     if model_set == 1:
         pass
     elif model_set == 2:
+        X = ['nil', 'nil', 'nil', 'nil', queue[0][3], queue[1][3], queue[0][1], queue[1][1], False, False]
+        y = 'sh'
+        print('x = {}, y = {}'.format(X,y))
         while len(queue) > 0:
-            if not stack:
-                X = ['nil', 'nil', 'nil', 'nil', queue[0][3], queue[1][3], queue[0][1], queue[1][1], False, False]
+            # if not stack:
+            #     print(len(stack))
+            #     X = ['nil', 'nil', 'nil', 'nil', queue[0][3], queue[1][3], queue[0][1], queue[1][1], False, False]
+            #     y = 'sh'
+            #     stack, queue, graph = shift(stack, queue, graph)
+            #     print(stack)
+            #     print('x = {}, y = {}'.format(X, y))
+            #     #lägg till saker i features
+            # else:
+            top = stack[0]
+            first = queue[0]
+            
+            top_id = int(top[0])
+            first_id = int(first[0])
+            #arc
+            arced = False
+            if (top_id, first_id) in graph:
+                arced = True
+                X = []
+                y = 'ra.{}'.format(first[7])
+                stack, queue, graph = shift(stack, queue, graph)
+            elif (first_id, top_id) in graph:
+                arced = True
+                #'can-la' = True
+                X = []
+                y = 'la.{}'.format(top[7])
+                stack, queue, graph = reduce(stack, queue, graph)
+            #reduce
+            if not arced:
+                reduced = False
+                for k in stack:
+                    k_id = int(k[0])
+                    if (k_id, first_id) in graph or (first_id, k_id) in graph:
+                        reduced = True
+                        stack, queue, graph = reduce(stack, queue, graph)
+                        X = []
+                        y = 're'
+                        #can-re = True
+                        #lägga till i features
+                    else:    
+                        pass
+            #shift
+            if not arced and not reduced:
+                # X = [top[4], ]
                 y = 'sh'
-                stack.append(queue.pop(0))
-                print('x = {}, y = {}'.format(X, y))
-                #lägg till saker i features
-            else:
-                top = stack[0]
-                first = queue[0]
-                
-                top_id = int(top[0])
-                first_id = int(first[0])
-
-                #arc
-                arced = False
-                if (top_id, first_id) in graph:
-                    arced = True
-                    stack.append(queue.pop(0))
-
-                elif (first_id, top_id) in graph:
-                    arced = True
-                    #'can-la' = True
-                    stack.pop(0)
-
-                #reduce
-                if not arced:
-                    reduced = False
-                    for k in stack:
-                        k_id = int(k[0])
-                        if (k_id, first_id) in graph or (first_id, k_id) in graph:
-                            pass
-                        else:
-                            reduced = True
-                            stack.pop(0)
-                            #can-re = True
-                            #lägga till i features
-                #shift
-                if not arced and not reduced:
-                    stack.append(queue.pop(0))
+                stack, queue, graph = shift(stack, queue, graph)
+            print('x = {}, y = {}'.format(X,y))
 
     else:
         pass    
 
-
     return features
 
+
+def shift(stack, queue, graph):
+    stack = [queue[0]] + stack
+    queue = queue[1:]
+    return stack, queue, graph
+
+def reduce(stack, queue, graph):
+    return stack[1:], queue, graph
 
 if __name__ == "__main__":
     first_set = ['POS_stack', 'POS_queue', 'word_stack', 'word_queue', 'can-la', 'can-re']
     second_set = ['POS_stack0', 'POS_stack1', 'word_stack0', 'word_stack1', 'POS_queue0', 'POS_queue1', 'word_queue0', 'word_queue1', 'can-la', 'can-re']
     third_set = ['POS_stack0', 'POS_stack1', 'word_stack0', 'word_stack1', 'POS_queue0', 'POS_queue1', 'word_queue0', 'word_queue1', 'can-la', 'can-re',
                 'POS_sentece_order', 'wordform_sentence_order']
-    stack = []
+    # stack = []
     graph = []
     
     train_file = '../lab4/swedish_talbanken05_train.conll'
@@ -74,8 +92,9 @@ if __name__ == "__main__":
             line = f.split()
             if line == []:
                 root = ['0', 'ROOT', 'ROOT', 'ROOT', 'ROOT', 'ROOT', '0', 'ROOT', '0', 'ROOT']
-                sentence.insert(0, root)
+                stack = [root]
                 features = extract(stack, sentence, graph, second_set, sentence, 2)
+                break
             else:
                 graph.append((int(line[6]), int(line[0])))
                 # can-la = tuple plats 0 > tuple plats 1
